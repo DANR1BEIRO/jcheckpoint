a3c981498b30d7b93582616110414bb61db049dd
DANR1BEIRO <danribeirodev@gmail.com>
2026-04-01 00:01:08 -0300

    feat: setup env variables and project dependencies cleanup closes #1

--- src/main/java/com/jcheckpoint/JcheckpointApplication.java
+++ src/main/java/com/jcheckpoint/JcheckpointApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JcheckpointApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JcheckpointApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JcheckpointApplication.class, args);
+    }
 
 }
