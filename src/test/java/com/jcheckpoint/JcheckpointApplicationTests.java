53194da18bdede24179f812353965350b6d213c4
DANR1BEIRO <danribeirodev@gmail.com>
2026-03-31 22:13:38 -0300

    feat: Estrutura base em Spring Boot para o JCheckpoint

---
+++ src/test/java/com/jcheckpoint/JcheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
